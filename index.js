14669b923c38b99d380601c95a88e3851e192aab
Josue Abraham <josuea@noukod.com>
2019-02-19 14:53:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ return Driver.assign({}, driver, {[key]: value});
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
-  let prop = key;
+  const prop = key;
   driver[prop] = value;
   return driver;
 }
 
 function deleteFromDriverByKey(driver, key){
-  let newdriver = Driver.assign({}, driver);
+  const newdriver = Driver.assign({}, driver);
   delete newdriver[key];
   return newdriver;
 }
